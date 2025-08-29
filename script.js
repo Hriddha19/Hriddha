907efcb4a28d7ba9529219d81e64b4d9b1fcad3a
Hriddha19 <s2gedits10k@gmail.com>
2025-08-29 12:06:18 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Simple greeting in console
+console.log("Welcome to my portfolio!");
+
+// Make header text fade in
+window.onload = function() {
+  document.querySelector("header").style.opacity = 1;
+};
